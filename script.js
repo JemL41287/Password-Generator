a8a1cab89d7b48bdccfe95543d0055fbb33a6775
Jennifer Mullin <jennifer.lindsey.mullin@gmail.com>
2019-12-30 16:46:12 -0700

    added copy to clipboard button to html and variables/prompts to script.js

--- script.js
+++ script.js
@@ -1,3 +1,112 @@
+var specialCharacters = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  "."
+];
+
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var lowerCaseCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+
+var upperCaseCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
+
+function passwordOptions() {
+  var length = parseInt(prompt(
+    "How many characters would you like your password to be?"
+  ));
+
+  if (isNaN(length) === true){
+    alert("Password length must be a number");
+    return;
+  }
+
+  if (length < 8) {
+    alert ("Password must be at least 8 characters");
+    return;
+  }
+
+  if (length > 128) {
+    alert ("Password must be less than 129 characters");
+    return;
+  }
+}
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
